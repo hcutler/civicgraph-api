0d0f4442aeec111940efa7ef9962e4ca2bf9c8d7
Hannah Cutler New <hcutler@seas.upenn.edu>
2016-07-21 16:50:01 -0400

    updated test script

--- api-test.py
+++ api-test.py
@@ -3,7 +3,7 @@ import urllib
 import requests
 import urllib2
 
-# Categories
+# Get Categories
 # Returns json object with all categories and their associated id.
 # URL: /api/categories
 # Method: GET
@@ -17,9 +17,9 @@ import urllib2
 # (2) 405 METHOD NOT ALLOWED
 # Sample Call:
 url = "http://civicgraph.io/api/categories"
-data = '{"query":{"categories":[{}]}'
+data = '{"query":{"categories":[{"id": 1}]}'
 
-response = requests.put(url, data=data)
+response = requests.get(url, data=data)
 if (response.ok):
   jData = json.loads(response.content)
   for key in jData:
@@ -28,16 +28,16 @@ else:
   print response.raise_for_status()
 
 
-# Connections
+# Get Connections
 # URL: /api/connections
 # Method: GET
 # URL Params
-## Required: None
-## Data Params: None
-## Success Response: 200
-## Error Response:
+# # Required: None
+# # Data Params: None
+# # Success Response: 200
+# # Error Response:
 # (1) 404 Client Error: NOT FOUND
-## OR
+# # OR
 # (2) 405 METHOD NOT ALLOWED
 
 ## Sample Call:
@@ -50,15 +50,15 @@ if (response.ok):
   jData = json.loads(response.content)
   for key in jData:
     print JData[key] #print all connection types
-  #   print jData[key]["Employment"] #print only Employment connections
-  #   print jData[key]["Collaboration"] #print only Collaboration connections
-  #   print jData[key]["Data"] #print only Data connections
-  #   print jData[key]["Funding"] #print only Funding connections
-  #   print jData[key]["Relation"] #print only Relation connections
+    print jData[key]["Employment"] #print only Employment connections
+    print jData[key]["Collaboration"] #print only Collaboration connections
+    print jData[key]["Data"] #print only Data connections
+    print jData[key]["Funding"] #print only Funding connections
+    print jData[key]["Relation"] #print only Relation connections
 else:
   print response.raise_for_status()
 
-# Get Entities
+# Get Get Entities
 # URL: /api/entities
 # Method: GET
 # URL Params
@@ -70,7 +70,7 @@ else:
 ## OR
 # (2) 405 METHOD NOT ALLOWED
 
-# ## Sample Call:
+## Sample Call:
 url = "http://civicgraph.io/api/entities"
 print url
 data = '{"query":{"nodes":{}}'
