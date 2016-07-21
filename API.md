07aa5475794c6d4e8f775f0ab31072202b7483c2
Hannah Cutler New <hcutler@seas.upenn.edu>
2016-07-21 11:54:26 -0400

    working on api

--- API.md
+++ API.md
@@ -1,56 +1,50 @@
-**Title**
+**Get Categories**
 ----
-  <_Additional information about your API call. Try to use verbs that match both request type (fetching vs modifying) and plurality (one vs multiple)._>
+  Returns json object with all categories in civic graph and their associated integer id.
 
 * **URL**
 
-  <_The URL Structure (path only, no root url)_>
+  /api/categories
 
 * **Method:**
-  
-  <_The request type_>
 
-  `GET` | `POST` | `DELETE` | `PUT`
+  `GET`
   
 *  **URL Params**
 
-   <_If URL params exist, specify them in accordance with name mentioned in URL section. Separate into optional and required. Document data constraints._> 
-
    **Required:**
  
-   `id=[integer]`
-
-   **Optional:**
- 
-   `photo_id=[alphanumeric]`
+   `id=[integer]` ##NONE???!??!?
 
 * **Data Params**
 
-  <_If making a post request, what should the body payload look like? URL Params rules apply here too._>
+  None
 
 * **Success Response:**
-  
-  <_What should the status code be on success and is there any returned data? This is useful when people need to to know what their callbacks should expect!_>
 
   * **Code:** 200 <br />
-    **Content:** `{ id : 12 }`
+    **Content:** {"categories": [{"id": 1, "name": "Smart & Resilient Cities"}, {"id": 2, "name": "Data & Analytics"}, {"id": 3, "name": "General Civic Tech"}, {"id": 4, "name": "Social Services"}, {"id": 5, "name": "Jobs & Education"}, {"id": 6, "name": "GovTech"}]}
+    
  
 * **Error Response:**
 
-  <_Most endpoints will have many ways they can fail. From unauthorized access, to wrongful parameters etc. All of those should be liste d here. It might seem repetitive, but it helps prevent assumptions from being made where they should be._>
-
-  * **Code:** 401 UNAUTHORIZED <br />
-    **Content:** `{ error : "Log in" }`
+  * **Code:** 404 NOT FOUND <br />
+    **Content:** `{ error : "User doesn't exist" }`
 
   OR
 
-  * **Code:** 422 UNPROCESSABLE ENTRY <br />
-    **Content:** `{ error : "Email Invalid" }`
+  * **Code:** 401 UNAUTHORIZED <br />
+    **Content:** `{ error : "You are unauthorized to make this request." }`
 
 * **Sample Call:**
 
-  <_Just a sample call to your endpoint in a runnable format ($.ajax call or a curl request) - this makes life easier and more predictable._> 
-
-* **Notes:**
-
-  <_This is where all uncertainties, commentary, discussion etc. can go. I recommend timestamping and identifying oneself when leaving comments here._> 
+  ```javascript
+    $.ajax({
+      url: "/users/1",
+      dataType: "json",
+      type : "GET",
+      success : function(r) {
+        console.log(r);
+      }
+    });
+  ```
--- api-test.py
+++ api-test.py
@@ -1,15 +1,95 @@
 import json
 import urllib
+import requests
 
-url = "https://civicinsight.azurewebsites.net/athena"
-response = urllib.urlopen(url)
-contents = response.read() #returns type 'string'
+# Categories
+# Returns json object with all categories and their associated id.
+# URL: /api/categories
+# Method: GET
+# URL Params
+## Required: 
+## Data Params: url, data
+## Success Response: 200
+## Error Response: 
+# (1) 404 Client Error: NOT FOUND
+###: requests.exceptions.HTTPError: 404 Client Error: NOT FOUND for url: http://civicgraph.io/api/categoriez
+# (2) 405 METHOD NOT ALLOWED
+## Sample Call:
+url = "http://civicgraph.io/api/categories"
+data = '{"query":{"categories":[{"id"}]}'
 
-text = contents.decode('utf8') #returns type 'unicode'
+response = requests.get(url, data=data)
+if (response.ok):
+  print response.content
+  jData = json.loads(response.content)
+  # for key in jData:
+  #   print jData[key]
+else:
+  print response.raise_for_status()
 
-all_data = json.loads(text) #returns type 'json'
 
-nodes = json.dumps(all_data['nodes'], indent=4)
-print nodes
+# connections
+# URL: /api/connections
+# Method: GET
+# URL Params
+## Required: 
+## Data Params:
+## Success Response: 200
+## Error Response: 405 METHOD NOT ALLOWED
+## Sample Call:
+
+# url = "http://civicgraph.io/api/connections"
+# data = '{"query":{"connections":{}}'
+# response = requests.get(url, data=data)
+
+# if (response.ok):
+#   jData = json.loads(response.content)
+#   for key in jData:
+#     print jData[key]
+# else:
+#   print response.raise_for_status()
+
+# entities
+# URL: /api/entities
+# Method: GET
+# URL Params
+## Required: 
+## Data Params:
+## Success Response:
+## Error Response:
+
+# requests.exceptions.ConnectionError: HTTPConnectionPool(host='civicgraph.io', port=80): Max retries exceeded with url: /api/categories (Caused by NewConnectionError('<requests.packages.urllib3.connection.HTTPConnection object at 0x10333bb10>: Failed to establish a new connection: [Errno 8] nodename nor servname provided, or not known',))
 
 
+# ## Sample Call:
+# url = "http://civicgraph.io/api/connections"
+# data = '{"query":{"nodes":{}}'
+# response = requests.get(url, data=data)
+
+# if (response.ok):
+#   jData = json.loads(response.content)
+#   for key in jData:
+#     print jData[key]
+# else:
+#   print response.raise_for_status()
+
+
+
+
+
+# data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+# response = requests.get(url, data=data)
+
+
+
+#OLD STUFF
+# url = "https://civicinsight.azurewebsites.net/athena"
+# response = urllib.urlopen(url)
+# contents = response.read() #returns type 'string'
+
+# text = contents.decode('utf8') #returns type 'unicode'
+
+# all_data = json.loads(text) #returns type 'json'
+
+# nodes = json.dumps(all_data['nodes'], indent=4)
+# print nodes
